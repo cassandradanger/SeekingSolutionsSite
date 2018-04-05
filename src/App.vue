d38b7b83415e49fa434f3a584c6ffd2efa1ec0e2
Casie Lynch <casie@primeacademy.io>
2018-04-04 19:58:42 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <div class="headerThing">
+      <img class="logo" src="./assets/SSlogoWhite.png"/>
+    </div>
+    <router-view/>
+    <div class="footerThing">
+      <img class="logoFooter" src="./assets/navigating.png"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 60px;
+}
+
+.headerThing{
+  padding: 20px;
+  height: 100px;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  color: white;
+  background-color: #22622e;
+}
+
+.footerThing{
+  padding: 20px;
+  height: 50px;
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  color: white;
+  background-color: #22622e;
+}
+
+.logo{
+  height: 100px;
+  margin-left: 40px;
+}
+
+.logoFooter{
+  position: absolute;
+  right: 50px;
+  bottom: 20px;
+  height: 50px;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="main">
+    <img class="marv" src="../assets/marv.png"/>
+    <div class="description">
+        <h3>Marv Siekman, CFP<span class="r">®</span></h3>
+        <p>Business Planning Consultant</p>
+        <p>320.894.5015</p>
+        <p><a href="mailto:marv@seekingsolutions.net">marv@seekingsolutions.net</a></p>
+        <p><a href="http://www.ascentmn.com/" target="_blank">www.ascentmn.com</a></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main'
+}
+</script>
+
+<style scoped>
+
+  .main{
+    margin-top: 200px;
+    text-align: center;
+  }
+
+  .description{
+    display: inline-block;
+    text-align: left;
+    margin: -10px 0 10px 20px;
+    vertical-align: top;
+  }
+
+  h3{
+    margin-top: 10px;
+  }
+
+  .marv{
+    height: 170px;
+    display: inline-block;
+  }
+
+  .r{
+    font-size: 8pt;
+    vertical-align: top;
+  }
+
+</style>
